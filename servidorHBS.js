67ad525763b9c6cd5f084ac7cf48d970bcf616c7
Juan Pablo David Barani <juanpablo_b77@hotmail.com>
2022-08-17 12:47:33 -0300

    [FIX 4]

--- servidorHBS.js
+++ servidorHBS.js
@@ -135,7 +135,7 @@ function auth(req, res, next){
 let avatar = multer.diskStorage({
   destination:(req,file,cb)=>{
     console.log('carpeta multer diskStorage')
-    cb(null,'/views/avatares')
+    cb(null,'views/avatares')
   },
   filename:(req,file,cb)=>{
     cb(null,`${Date.now()}-${file.originalname}`)
